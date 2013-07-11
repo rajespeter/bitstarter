4c340b249fffce784d20a4d17b0fc3038931a639
Rajesh Peter <rajeshpeter@gmail.com>
2013-07-11 09:36:52 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,20 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+var fileName = "index.html";
+
+var content;
+
+fs.readFile(fileName, function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
